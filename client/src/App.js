9b5c7babb666c477617515e9e4744125a6c67b27
ghislain.anciaux <ghislain.anciaux@internet.lu>
2021-07-07 16:55:56 +0200

    Start define components Form, Posts, Post

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { Container, AppBAr, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from './images/memories.png'
+
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxidth="lg">
+            <AppBar postion="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60"></img>
+                </AppBar>
+                <Grow in>
+                    <Container>
+                        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                            <Grid item xs={12} sm={7}></Grid>
+                                <Posts></Posts>
+                            <Grid item xs={12} sm={4}></Grid>
+                            <Form></Form>
+                        </Grid>
+                    </Container>
+                </Grow>
+        </Container>
     );
 }
 
