3e2ae2d0c8f1c46b97cd9b50f60c5bb604a18d40
ganciaux <ghislain.anciaux@live.fr>
2021-07-07 07:56:54 +0200

    Add post model, components (form, post)

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Container, AppBAr, Typography, Grow, Grid } from '@material-ui/core';
 const App = () => {
     return (
         <div>
--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://ganciaux:ganciaux@cluster0.acqzd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://ganciaux:ganciaux@cluster0.acqzd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb://localhost:27017/jsmastery';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
